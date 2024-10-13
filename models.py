cf61eec504b29452438a974a8f9b6ce3e3af0506
Mariia Leonova <massha.leonova@yandex.ru>
2024-10-13 22:16:54 +0300

    initial commit

--- models.py
+++ models.py
@@ -51,10 +51,10 @@ class Swapi(Base):
 
 async def init_models():
     async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
 
-asyncio.run(init_models())
+
 
